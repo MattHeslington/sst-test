233c5fe52dfd31c0ecd5c73fef1ff8e3f332fb88
Matt Heslington <matt.heslington@icloud.com>
2023-06-29 12:40:51 +0800

    chore(package.json): add test script feat(core): add cost calculation function and tests
    chore(package.json): rearrange devDependencies and add stripe as a dependency
    feat(billing.js): add billing functionality using Stripe API
    feat(ApiStack.js): add environment variable for STRIPE_SECRET_KEY and new route for billing endpoint
    
    test(StorageStack.test.js): add test for StorageStack

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -10,6 +10,9 @@ export function ApiStack({ stack, app }) {
 			authorizer: 'iam',
 			function: {
 				bind: [table]
+			},
+			environment: {
+				STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
 			}
 		},
 		routes: {
@@ -17,7 +20,8 @@ export function ApiStack({ stack, app }) {
 			'GET /notes/{id}': 'packages/functions/src/get.main',
 			'GET /notes': 'packages/functions/src/list.main',
 			'PUT /notes/{id}': 'packages/functions/src/update.main',
-			'DELETE /notes/{id}': 'packages/functions/src/delete.main'
+			'DELETE /notes/{id}': 'packages/functions/src/delete.main',
+			'POST /billing': 'packages/functions/src/billing.main'
 		}
 	})
 
